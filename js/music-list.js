296ee7914613cdc6a198654d1ca5015115aa4a58
proficientmh <87502934+proficientmh@users.noreply.github.com>
2021-08-26 03:59:28 +0600

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,91 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [{
+        name: "1. קחי נשימה / גל שריג  ",
+        artist: "",
+        img: "music-1",
+        src: "music-2"
+    },
+    {
+        name: "2. באהבת נעורים / רחל ברוך",
+        artist: "",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "3. מקסיקו / אזב עדן זמרו וגל שריג",
+        artist: "",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "4.למה? / גל שריג",
+        artist: "",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "5.בדידות / גל שריג",
+        artist: "",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "6.שלנו / אזב עדן זמרו וגל שריג",
+        artist: "",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "7.קחי נשימה / גל שריג<",
+        artist: "",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "8.באהבת נעורים / רחל ברוך",
+        artist: "",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "9.מקסיקו / אזב עדן זמרו וגל שריג",
+        artist: "",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "10.למה? / גל שריג",
+        artist: "",
+        img: "music-10",
+        src: "music-10"
+    },
+    {
+        name: "11.בדידות / גל שריג",
+        artist: "",
+        img: "music-11",
+        src: "music-11"
+    },
+    {
+        name: "12.שלנו / אזב עדן זמרו וגל שריג",
+        artist: "",
+        img: "music-12",
+        src: "music-12"
+    },
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+];
